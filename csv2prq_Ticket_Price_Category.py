014ea88a4c6a1aec6e52543e6969ff79ff952ff2
Nanic38 <129212294+Nanic38@users.noreply.github.com>
2025-04-22 16:27:27 +0530

    Add files via upload

---
+++ csv2prq_Ticket_Price_Category.py
@@ -0,0 +1,47 @@
+import sys
+import logging 
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_date
+from datetime import datetime
+from pyspark.sql.functions import input_file_name
+from pyspark.sql import functions as F  # Add this import
+
+# Create a GlueContext
+glueContext = GlueContext(SparkContext.getOrCreate())
+
+# Initialize Spark session
+spark = glueContext.spark_session
+
+# Get job parameters
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_SOURCE_PATH', 'S3_OUTPUT_PATH'])
+
+# Extract parameters
+source_path = args['S3_SOURCE_PATH']
+output_path = args['S3_OUTPUT_PATH']
+
+# Read CSV data from source patht
+try:
+    source_data = spark.read.csv(source_path, header=True, inferSchema=True)
+    logging.info ("Data read successfully from source path")
+except Exception as e:
+    logging.error(f"Error reading data from source path: {e}")
+    sys.exit(1)
+
+# Add a new column with the current date in the filename
+current_date_str = datetime.now().strftime('%Y%m%d')
+
+# Extract the source file name from the input_file_name
+source_data_with_filename = source_data.withColumn("source_filename", input_file_name())
+
+# Extract the base name of the source file without the extension
+source_data_with_filename = source_data_with_filename.withColumn("source_filename", F.expr("substring_index(substring_index(source_filename, '/', -1), '.', 1)"))
+
+# Create the output file name
+source_data_with_filename = source_data_with_filename.withColumn("output_filename", F.concat(F.col("source_filename"), F.lit("_"), F.lit(current_date_str), F.lit(".parquet")))
+
+# Write the data to a single Parquet file with the new filename
+source_data_with_filename.coalesce(1).write.parquet(output_path + "/" + current_date_str, mode='overwrite')
---
+++ csv2prq_documtry-multi.py
@@ -0,0 +1,61 @@
+import sys
+import logging
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_date
+from datetime import datetime
+from pyspark.sql.functions import input_file_name
+from pyspark.sql import functions as F  # Add this import
+
+# Create a GlueContext
+glueContext = GlueContext(SparkContext.getOrCreate())
+
+# Initialize Spark session
+spark = glueContext.spark_session
+
+# Get job parameters
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_SOURCE_PATH', 'S3_OUTPUT_PATH', 'S3_FILE_NAME'])
+
+# Extract parameters
+source_path = args['S3_SOURCE_PATH']
+output_path = args['S3_OUTPUT_PATH']
+source_filename = source_path + args['S3_FILE_NAME']
+file_name = args['S3_FILE_NAME']
+
+# Read CSV data from source path
+try:
+    source_data = spark.read.csv(source_filename, header=True, inferSchema=True)
+    logging.info ("Data read successfully from source path")
+except Exception as e:
+    logging.error(f"Error reading data from source path: {e}")
+    sys.exit(1)    
+
+# Add a new column with the current date in the filename
+current_date_str = datetime.now().strftime('%Y%m%d')
+
+# Extract the source file name from the input_file_name
+source_data_with_filename = source_data.withColumn("source_filename", input_file_name())
+
+# Extract the base name of the source file without the extension
+source_data_with_filename = source_data_with_filename.withColumn("source_filename", F.expr("substring_index(substring_index(source_filename, '/', -1), '.', 1)"))
+
+# Create the output file name
+source_data_with_filename = source_data_with_filename.withColumn("output_filename", F.concat(F.col("source_filename"), F.lit("_"), F.lit(current_date_str), F.lit(".parquet")))
+
+# Write the data to a single Parquet file with the new filename
+source_data_with_filename.coalesce(1).write.parquet(output_path + "/" + current_date_str, mode='append')
+
+# rename created file
+hadoopPath = SparkContext._jvm.org.apache.hadoop.fs.Path(output_path)
+hadoopFs = hadoopPath.getFileSystem(SparkContext._jvm.org.apache.hadoop.conf.Configuration())
+statuses = hadoopFs.globStatus(hadoopPath)
+
+file_name = [file.getPath().getName() for file in statuses if file.getPath().getName().startswith('part-')][0]
+
+hadoopFs.rename(SparkContext._jvm.org.apache.hadoop.fs.Path({output_path}), SparkContext._jvm.org.apache.hadoop.fs.Path(output_path + ".parquet"))
+#fs.rename(
+#    output_path,
+#    Path(file_path + "{file_name}.parquet"))
